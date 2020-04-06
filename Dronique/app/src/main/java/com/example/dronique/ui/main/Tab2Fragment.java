6f5c67265de16aee301a6f3cf152f0bcbf42283c
NIEMEZYK Teddy <niemezyk.teddy@gmail.com>
2020-04-06 15:07:21 +0200

    Add new icon for marker + get asimuth

--- Dronique/app/src/main/java/com/example/dronique/ui/main/Tab2Fragment.java
+++ Dronique/app/src/main/java/com/example/dronique/ui/main/Tab2Fragment.java
@@ -1,20 +1,23 @@
 package com.example.dronique.ui.main;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.service.autofill.FieldClassification;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
-import com.example.dronique.Client;
 import com.example.dronique.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -27,24 +30,49 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.Map;
-
 public class Tab2Fragment extends Fragment implements SensorEventListener {
 
     private MapView mMapView;
     private GoogleMap mGoogleMap;
     private Marker mMarker;
+    private Drone mDrone;
 
     private SensorManager mSensorManager;
-    private Sensor mAccel;
+    private Sensor mAccelerometer;
+    private Sensor mMagneticField;
+
+    private BitmapDrawable mBitmapDrawable =  null;
+    private Bitmap mBitmap = null;
+    private int mDefaultRotation = -43;
+
+    Thread mThread;
+
+    float[] magneticVector = new float[3];
+    float[] accelerometerVector = new float[3];
+    float[] resultMatrix = new float[9];
+    float[] values = new float[3];
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_two, container, false);
 
-        // Gestion de la MapView
-        mMapView = (MapView) view.findViewById(R.id.map);
+        mDrone = new Drone();
+
+        //Gestion de la bitmap
+        mBitmapDrawable = (BitmapDrawable) getResources().getDrawable(R.drawable.my_marker);
+        mBitmap = Bitmap.createScaledBitmap(mBitmapDrawable.getBitmap(), 75, 75, false);
+
+        mThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mMarker.setPosition();
+            }
+        });
+
+                // Gestion de la MapView
+                mMapView = (MapView) view.findViewById(R.id.map);
         mMapView.onCreate(savedInstanceState);
         mMapView.onResume();
 
@@ -61,13 +89,17 @@ public class Tab2Fragment extends Fragment implements SensorEventListener {
                 mGoogleMap = googleMap;
                 LatLng posLaRochelle = new LatLng(46.1558,-1.1532);
                 mMarker = mGoogleMap.addMarker(new MarkerOptions().position(posLaRochelle));
+                mMarker.setIcon(BitmapDescriptorFactory.fromBitmap(mBitmap));
+                mMarker.setRotation(mDefaultRotation);
                 CameraPosition cameraPosition = new CameraPosition.Builder().target(posLaRochelle).zoom(12).build();
                 googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
             }
         });
 
         mSensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
-        mAccel = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mMagneticField = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
 
         return view;
@@ -78,9 +110,9 @@ public class Tab2Fragment extends Fragment implements SensorEventListener {
     public void onResume() {
         super.onResume();
         mMapView.onResume();
-        mSensorManager.registerListener(this, mAccel, 10);
+        mSensorManager.registerListener(this, mAccelerometer, 1000000000);
+        mSensorManager.registerListener(this, mMagneticField, 1000000000);
     }
-
 
     @Override
     public void onPause() {
@@ -104,11 +136,78 @@ public class Tab2Fragment extends Fragment implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        Log.w("Accelerometer : ", "X -> " + event.values[0]);
-        Log.w("Accelerometer : ", "Y -> " + event.values[1]);
-        Log.w("Accelerometer : ", "Z -> " + event.values[2]);
 
-        update(event.values[0], event.values[1], event.values[2]);
+        if(event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            magneticVector = event.values;
+        }
+
+        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+        {
+            accelerometerVector = event.values;
+        }
+
+        SensorManager.getRotationMatrix(resultMatrix, null, accelerometerVector, magneticVector);
+        SensorManager.getOrientation(resultMatrix, values);
+        int asimuth = (int)Math.toDegrees(values[0]);
+        Log.w("ORIENTATION", "asimuth : " +  asimuth);
+
+        if(mMarker != null) {
+            mMarker.setRotation((float) Math.toDegrees(values[0]) + mDefaultRotation);
+
+            double kmPerHour = 0;
+            if(accelerometerVector[1] <= 0)
+                kmPerHour = 111.11;
+            else
+                kmPerHour = 111.11 - ((accelerometerVector[1] * 10 * 111.11) / 90);
+
+            // longitude
+            double kmPerDegreeLng = 111.11 * (double)Math.cos((int) mMarker.getPosition().longitude);
+            double longitudeAdded = kmPerHour / kmPerDegreeLng;
+
+            // lattitude
+            double kmPerDegreeLat = 111.11;
+            double lattitudeAdded = kmPerHour / kmPerDegreeLat;
+
+
+            double newLat = 0;
+            double newLng = 0;
+            double lngPercentage = 0;
+            double latPercentage = 0;
+            if(0 < asimuth && asimuth <= 90) {
+                lngPercentage = asimuth/90*100;
+                latPercentage = 100 - lngPercentage;
+
+                newLat = mMarker.getPosition().latitude + latPercentage * lattitudeAdded;
+                newLng = mMarker.getPosition().longitude + lngPercentage * longitudeAdded;
+            }
+            else if(90 < asimuth && asimuth <= 180) {
+                lngPercentage = (asimuth + 90) /180 * 100;
+                latPercentage = 100 - lngPercentage;
+
+                newLat = mMarker.getPosition().latitude - latPercentage * lattitudeAdded;
+                newLng = mMarker.getPosition().longitude + lngPercentage * longitudeAdded;
+            }
+            else if(-179 <= asimuth && asimuth < -90) {
+                lngPercentage = (asimuth + 180) /90 * 100;
+                latPercentage = 100 - lngPercentage;
+
+                newLat = mMarker.getPosition().latitude - latPercentage * lattitudeAdded;
+                newLng = mMarker.getPosition().longitude - lngPercentage * longitudeAdded;
+            }
+            else if(-90 < asimuth && asimuth <= 0) {
+                lngPercentage = (asimuth +90) /90 * 100;
+                latPercentage = 100 - lngPercentage;
+
+                newLat = mMarker.getPosition().latitude + latPercentage * lattitudeAdded;
+                newLng = mMarker.getPosition().longitude - lngPercentage * longitudeAdded;
+            }
+
+            mThread.
+
+            mMarker.setPosition(new LatLng(newLat, newLng));
+            CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(newLat, newLng)).zoom(12).build();
+            mGoogleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        }
     }
 
     @Override
@@ -116,7 +215,4 @@ public class Tab2Fragment extends Fragment implements SensorEventListener {
 
     }
 
-    public void update(double x, double y, double z) {
-
-    }
 }
